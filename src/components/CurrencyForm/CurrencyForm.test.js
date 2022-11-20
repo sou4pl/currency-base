6435bcf246ae0a34c0f0cf3accf222f75e99a208
Michał Słota <michal.k.slota@gmail.com>
2022-11-20 16:05:07 +0100

    add currencyForm tests

---
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -0,0 +1,40 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import CurrencyForm from './CurrencyForm';
+import userEvent from '@testing-library/user-event';
+
+
+describe('Component CurrencyForm', () => {
+  it('should render without crashing', () => {
+    render(<CurrencyForm action={() => {}} />);
+  });
+  it('should run action callback with proper data on form submit', () => {
+    
+    const testCases = [
+      { amount: '100', from: 'PLN', to: 'USD' },
+      { amount: '20', from: 'USD', to: 'PLN' },
+      { amount: '200', from: 'PLN', to: 'USD' },
+      { amount: '345', from: 'USD', to: 'PLN' },
+];
+
+for(const testObj of testCases) {
+  const action = jest.fn();
+    render(<CurrencyForm action={action} />);
+
+    const submitButton = screen.getByText('Convert');
+    const amountField = screen.getByTestId('amount');
+    const fromSelector = screen.getByTestId('from');
+    const toSelector = screen.getByTestId('to');
+
+    userEvent.type(amountField, testObj.amount);
+    userEvent.selectOptions(fromSelector, testObj.from);
+    userEvent.selectOptions(toSelector, testObj.to);
+
+    userEvent.click(submitButton);
+
+    expect(action).toHaveBeenCalledTimes(1);
+    expect(action).toHaveBeenCalledWith({ amount: parseInt(testObj.amount), from: testObj.from, to: testObj.to });
+
+    cleanup()
+}
+  });
+});
